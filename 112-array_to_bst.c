2f58377ed0307a407412a75188a5c50d7bb80509
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-09-21 19:12:58 +0100

    Task 26

---
+++ 112-array_to_bst.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a binary search tree from array
+ * @array: array of integers
+ * @size:  size of array
+ * Return: pointer to root node
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root;
+	size_t i;
+
+	if (size == 0)
+		return (NULL);
+
+	root = bst_insert(&root, array[0]);
+
+	for (i = 1; i < size; i++)
+		bst_insert(&root, array[i]);
+
+	return (root);
+}
