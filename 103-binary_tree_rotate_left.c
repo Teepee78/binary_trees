efce1cc2c89be05886117f18fa3d4e646177480d
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-09-21 11:11:41 +0100

    Update task 22

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -7,6 +7,7 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *tree_parent = tree->parent;
 	binary_tree_t *tree_right = tree->right;
 
 	if (tree == NULL)
@@ -14,6 +15,14 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 
 	tree_right->parent = tree->parent;
 	tree->parent = tree_right;
+	/* check if tree is right or left node */
+	if (tree_parent != NULL)
+	{
+		if (tree->parent->right == tree)
+			tree->parent->right = tree_right;
+		else if (tree->parent->left == tree)
+			tree->parent->left = tree_right;
+	}
 	tree->right = tree_right->left;
 	tree_right->left = tree;
 
